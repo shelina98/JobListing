4c312b9fda3a4d2b24b5365c2ee107a5204bb2bb
shelina98 <shelaveliu1@gmail.com>
2022-08-25 13:20:24 +0200

    Recruiter SHELL

--- src/app/Recruiter/recruiter-routing-module.ts
+++ src/app/Recruiter/recruiter-routing-module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {RecruiterDashboardComponent} from "./recruiter-dashboard/recruiter-dashboard.component";
+import {PageNotFoundComponent} from "../page-not-found/page-not-found.component";
+import {RecruiterShellComponent} from "./recruiter-shell/recruiter-shell.component";
 
 
 const routes: Routes = [
@@ -8,10 +10,10 @@ const routes: Routes = [
     path: 'recruiter',
     component: RecruiterDashboardComponent,
     children: [
-      // {
-      //   path: '',
-      //   component:JobShellComponent
-      // },
+      {
+        path: '',
+        component:RecruiterShellComponent
+      },
     ]
   }
 ]
